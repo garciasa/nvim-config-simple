db9ab892bd311bccd50a2010a9f5aab01c0dcee7
garciasa <garciasa@gmail.com>
2024-03-10 11:20:25 +0000

    new nvim config simple

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,64 @@
+---@diagnostic disable: undefined-global
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "astro",
+          "templ",
+          "emmet_ls",
+          "tailwindcss",
+          "tsserver",
+          "html",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.astro.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.templ.setup({
+        capabilities = capabilities,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      vim.filetype.add({ extension = { templ = "templ" } })
+    end,
+  },
+}
