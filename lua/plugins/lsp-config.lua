01da22e909fbd1516ace43590b5018055b33cb6b
garciasa <garciasa@gmail.com>
2024-03-30 12:02:56 +0000

    adding shift to move up and down faster

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -57,7 +57,7 @@ return {
         capabilities = capabilities,
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "H", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,3 +12,16 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<TAB>', ':bnext<CR>')
+vim.keymap.set('n', '<s-TAB>', ':bprev<CR>')
+vim.keymap.set('n', '<leader>bc', ':bw<CR>')
+
+-- Shift line  up or down
+vim.keymap.set("v", "<s-k>", ":m-2<CR>gv=gv")
+vim.keymap.set("v", "<s-j>", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "<s-k>", "5k", {noremap = true})
+vim.keymap.set("n", "<s-j>", "5j", {noremap = true})
+
+
